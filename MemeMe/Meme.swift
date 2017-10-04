3179c646e2573d55426a9103076b5df413a24793
Sebastian <sebastian.prokesch@gmail.com>
2017-10-04 23:12:10 +0200

    added blured background imageview added outlets to viewcontroller blur effect and setting the image

---
+++ MemeMe/Meme.swift
@@ -0,0 +1,27 @@
+    //
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Sebastian on 04.10.17.
+//  Copyright © 2017 IdrilsBlog. All rights reserved.
+//
+
+import Foundation
+import UIKit
+    
+struct Meme {
+    let topText: String
+    let bottomText: String
+    let originalImage: UIImage
+    let memedImage: UIImage
+    
+    init(topText: String, bottomText:String, originalImage: UIImage, memedImage: UIImage) {
+        self.topText = topText
+        self.bottomText = bottomText
+        self.originalImage = originalImage
+        self.memedImage = memedImage
+    }
+    
+  
+    
+}
--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -9,17 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate,
-UINavigationControllerDelegate {
+UINavigationControllerDelegate, UITextFieldDelegate {
 
-
+    @IBOutlet weak var imageViewBackground: UIImageView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var bottomText: UITextField!
+    @IBOutlet weak var topText: UITextField!
+    
+    
+    let memeTextAttributes:[String:Any] = [
+        NSAttributedStringKey.strokeColor.rawValue: UIColor.black,
+        NSAttributedStringKey.foregroundColor.rawValue: UIColor.white,
+        NSAttributedStringKey.font.rawValue: UIFont(name: "Impact", size: 40)!,
+        NSAttributedStringKey.strokeWidth.rawValue: -3.0,]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        bottomText.defaultTextAttributes = memeTextAttributes
+        bottomText.text = "BOTTOM"
+        bottomText.delegate = self
+        
+        topText.defaultTextAttributes = memeTextAttributes
+        topText.text = "TOP"
+        topText.delegate = self
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: Helper Functions
+    func blurImageView(imageViewToBlur: UIImageView) {
+        imageViewToBlur.backgroundColor = UIColor.clear
+        
+        let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        //always fill the view
+        blurEffectView.frame = imageViewToBlur.bounds
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        imageViewToBlur.addSubview(blurEffectView)
     }
     
     //MARK: Actions
@@ -47,8 +79,18 @@ UINavigationControllerDelegate {
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            self.imageView.image = image
+            // Add second Image View in the background with blur
+            self.imageViewBackground.image = image
+            blurImageView(imageViewToBlur: self.imageViewBackground)
+        }
         dismiss(animated: true, completion: nil)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
     
 }
