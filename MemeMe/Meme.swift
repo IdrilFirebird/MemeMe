950539ecfecb2a3108446625451349bcdc2865e8
Sebastian <sebastian.prokesch@gmail.com>
2017-10-07 13:31:47 +0200

    added share button and function added keyboard push view away function

--- MemeMe/Meme.swift
+++ MemeMe/Meme.swift
@@ -22,6 +22,7 @@ struct Meme {
         self.memedImage = memedImage
     }
     
+    
   
     
 }
--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -16,9 +16,18 @@ UINavigationControllerDelegate, UITextFieldDelegate {
     @IBOutlet weak var bottomText: UITextField!
     @IBOutlet weak var topText: UITextField!
     
+    @IBOutlet weak var bottomToolbar: UIToolbar!
+    @IBOutlet weak var topToolbar: UIToolbar!
+    
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var cameraPickerButton: UIBarButtonItem!
+    @IBOutlet weak var libraryPickerButton: UIBarButtonItem!
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    
     
     let memeTextAttributes:[String:Any] = [
-        NSAttributedStringKey.strokeColor.rawValue: UIColor.black,
+    
+    NSAttributedStringKey.strokeColor.rawValue: UIColor.black,
         NSAttributedStringKey.foregroundColor.rawValue: UIColor.white,
         NSAttributedStringKey.font.rawValue: UIFont(name: "Impact", size: 40)!,
         NSAttributedStringKey.strokeWidth.rawValue: -3.0,]
@@ -26,8 +35,10 @@ UINavigationControllerDelegate, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.insetsLayoutMarginsFromSafeArea  = true
-//        pickImageFromCameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(.Camera)
-//        pickImageFromAlbumButton.enabled = UIImagePickerController.isSourceTypeAvailable(.PhotoLibrary)
+        cameraPickerButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        libraryPickerButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.photoLibrary)
+        shareButton.isEnabled = false
+        cancelButton.isEnabled = false
         
         bottomText.defaultTextAttributes = memeTextAttributes
         bottomText.text = "BOTTOM"
@@ -40,9 +51,14 @@ UINavigationControllerDelegate, UITextFieldDelegate {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
     }
     
     //MARK: Helper Functions
@@ -57,6 +73,35 @@ UINavigationControllerDelegate, UITextFieldDelegate {
         
         imageViewToBlur.addSubview(blurEffectView)
     }
+    
+    func memeMefyImage() -> UIImage {
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        
+        hideToolBars(true)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        
+        hideToolBars(false)
+        return memedImage
+    }
+    
+    func hideToolBars(_ hide: Bool)  {
+        topToolbar.isHidden = hide
+        bottomToolbar.isHidden = hide
+    }
+    
+    func save(_ memedImage: UIImage?) {
+        let meme: Meme
+        
+        if let memedImage = memedImage {
+            meme = Meme(topText: topText.text!, bottomText: bottomText.text!, originalImage: imageView.image!, memedImage: memedImage)
+        } else {
+            meme = Meme(topText: topText.text!, bottomText: bottomText.text!, originalImage: imageView.image!, memedImage: memeMefyImage() )
+        }
+    }
+    
+  
+    
     
     //MARK: Actions
 
@@ -75,6 +120,13 @@ UINavigationControllerDelegate, UITextFieldDelegate {
         present(imagePicker, animated: true, completion: nil)
     }
     
+    @IBAction func shareButtonAction(_ sender: Any) {
+        let memedImage = memeMefyImage()
+        let activityController = UIActivityViewController(activityItems: [memedImage], applicationActivities: nil)
+        
+        present(activityController, animated: true, completion: {self.save(memedImage)})
+    }
+    
     
     //MARK: Delegate Methods
     
@@ -89,12 +141,42 @@ UINavigationControllerDelegate, UITextFieldDelegate {
             self.imageViewBackground.image = image
             blurImageView(imageViewToBlur: self.imageViewBackground)
         }
+        self.shareButton.isEnabled = true
         dismiss(animated: true, completion: nil)
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+    
+    //MARK: Keyboard handling
+    @objc func keyboardWillShow(_ notification:Notification) {
+        
+        view.frame.origin.y = 0 - getKeyboardHeight(notification)
+    }
+    
+    @objc func keyboardWillHide(_ notification:Notification) {
+        view.frame.origin.y = 0
+    }
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillHide , object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
     }
     
 }
